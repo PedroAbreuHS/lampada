20916fca6514bdf8d3579ab592255e6773f47623
PedroAbreuHS <104274261+PedroAbreuHS@users.noreply.github.com>
2023-06-01 07:52:42 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+var lamp = document.getElementById('lampada');
+
+function ligar() {
+    lamp.src = 'imagens/lampada_ligada.svg'
+}
+
+function desligar() {
+    lamp.src = 'imagens/lampada_desligada.svg'
+}
+
+function quebrar() {
+    lamp.src = 'imagens/lampada_quebrada.svg'
+}
